0d5423bba20e04a70f16ea98c0a25cc25e5b3b68
mecheline <ucheamaechi2016@gmail.com>
2025-01-25 03:52:42 +0100

    key swap

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider publishableKey={process.env.CLERK_SECRET_KEY}>
       <html lang="en">
         <body className="px-4">
           <ClerkLoading>
